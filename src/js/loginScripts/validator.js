2672cc0da14ee4cb44a82dc9db635739cc8bc307
GabrielSilvaD3v <123119325+GabrielD3Vsilva@users.noreply.github.com>
2024-04-09 00:39:37 -0300

    async functions

--- src/js/loginScripts/validator.js
+++ src/js/loginScripts/validator.js
@@ -2,8 +2,8 @@ import messagesToUsers from "../messagesToUsers.js";
 
 const existingData = JSON.parse(localStorage.getItem("myData")) || [];
 
-function objectExists (emailProps, passwordProps) {
-    return existingData.some(object => {
+async function objectExists (emailProps, passwordProps) {
+    return await existingData.some(object => {
         return object.emailProps === emailProps &&
         object.passwordProps === passwordProps
     })
@@ -25,11 +25,12 @@ function checkIfInputsIsEmpty ( ) {
     }
 }
 
-function checkIfUserExists ( ) {
+async function checkIfUserExists ( ) {
     const email = document.getElementById('email');
     const password = document.getElementById('password');
 
-    if(objectExists(email.value, password.value)) {
+    
+    if(await objectExists(email.value, password.value)) {
         messagesToUsers.caseUserExists( );
         email.value = '';
         password.value = '';
@@ -37,6 +38,7 @@ function checkIfUserExists ( ) {
         
         messagesToUsers.caseIncorrectPassword( );
     }
+    
     
 }
 
